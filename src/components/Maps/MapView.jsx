1696332095f57537ab997ce8f888d90fcd032ff2
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-17 18:52:20 +0100

    map working

--- src/components/Maps/MapView.jsx
+++ src/components/Maps/MapView.jsx
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const MapView = () => {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map', 
-      style: 'mapbox://styles/mapbox/streets-v12', 
-      center: [-74.5, 40], 
-      zoom: 9, 
-    });
-  }, []);
+    const [map, setMap] = useState(null);
+  
+    useEffect(() => {
+      if (!map) {
+        const bounds = new mapboxgl.LngLatBounds();
+        bounds.extend([-0.432054, 39.495578]);
+        bounds.extend([-0.379526, 39.475606]);
+        const newMap = new mapboxgl.Map({
+          container: 'map', 
+          style: 'mapbox://styles/mapbox/streets-v12', 
+          zoom: 15, 
+          maxBounds: bounds 
+        });
+        setMap(newMap);
+      }
+    }, [map]);
+    
+    return <div id="map" style={{height: "85vh", width: "100vw"}} ></div>;
+  };
 
-  return <div id="map"></div>;
-};
+
+  
 
 export default MapView;
+
