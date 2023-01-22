904856846c2288055893e952ab18777f19c9ddc8
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-22 12:20:11 +0100

    chatbot cors fixeado, las llamadas a la api se hacen desde el propio componente

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,30 +1,67 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { askQuestion } from '../../features/chatbot/chatbotService';
-
+import axios from 'axios';
 
 function Chatbot() {
-  const [question, setQuestion] = useState('');
-  const response = useSelector(state => state.chatbot.response);
-  const dispatch = useDispatch();
+  const [comment, setComment] = useState('');
+  const [response, setResponse] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(askQuestion({ question }));
+    const data = { comment };
+    try {
+      const res = await axios.post('https://cors-anywhere.herokuapp.com/https://chatbotapi-production-dbc1.up.railway.app/chat/', data);
+      setResponse(res.data.response);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
       />
       <button type="submit">Enviar</button>
-      <p>Respuesta: {response}</p>
+      <p>{response}</p>
     </form>
   );
 }
 
 export default Chatbot;
 
+
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { askQuestion } from '../../features/chatbot/chatbotService';
+// import { useSelector } from 'react-redux'
+
+// function Chatbot() {
+//   const [question, setQuestion] = useState('');
+//   const response = useSelector(state => state.chatbot.response);
+//   const status = useSelector(state => state.chatbot.status);
+//   const error = useSelector(state => state.chatbot.error);
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     dispatch(askQuestion({ question }));
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         value={question}
+//         onChange={(e) => setQuestion(e.target.value)}
+//       />
+//       <button type="submit">Enviar</button>
+//       {status === 'loading' && <p>Loading...</p>}
+//       {status === 'succeeded' && <p>Respuesta: {response}</p>}
+//       {status === 'failed' && <p>Error: {error}</p>}
+//     </form>
+//   );
+// }
+
+// export default Chatbot;
