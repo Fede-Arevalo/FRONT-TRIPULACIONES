4c5e1e08f681d889fde8a59ab20c6901ed21ee95
Fede-Arevalo <arevalo.feder@gmail.com>
2023-01-23 16:08:41 +0100

    elimino componente header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
@@ -28,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+       
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
