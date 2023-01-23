8cc42996669aeb71e2bea4e71caf645e7a5d2aeb
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-23 18:03:06 +0100

    corregido addIncidents

--- src/components/AddIncident/AddIncident.jsx
+++ src/components/AddIncident/AddIncident.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button, notification, Form, Input, Select } from "antd";
+import { Button, notification } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { CloudUploadOutlined } from "@ant-design/icons";
@@ -31,65 +31,72 @@ const AddIncident = () => {
     e.preventDefault();
     const formData = new FormData();
 
-    if (e.target.imagePost.files[0])
-      formData.set("imageIncident", e.target.imagePost.files[0]);
-    formData.set("incidentDate", e.target.incidentDate.value);
-    formData.set("timeIncident", e.target.timeIncident.value);
+    if (e.target.imageIncident.files[0])
+      formData.set("imageIncident", e.target.imageIncident.files[0]);
     formData.set("category", e.target.category.value);
+    formData.set("locationIncident", e.target.locationIncident.value);
     formData.set("title", e.target.title.value);
     formData.set("description", e.target.description.value);
-    formData.set("locationIncident", e.target.locationIncident.value);
 
     dispatch(createIncident(formData));
+    setTimeout(() => {
+      navigate("/incidents");
+    }, 2000);
   };
 
   return (
     <div className="addIncident">
       <div>
         <form className="addIncident-form" onSubmit={onSubmit}>
-          <h1>Rellene los datos para poder publicar</h1>
+          <h1>RELLENE LOS DATOS PARA PODER PUBLICAR</h1>
 
-          <Form.Item label="Fecha del incidente">
-            <Input type="date" name="incidentDate" placeholder="Fecha del incidente" required />
-          </Form.Item>
+          <div className="category">
+            <label>Categoría: </label>
+            <select name="category">
+              <option value="jardineria">Jardinería</option>
+              <option value="movilidad-sostenible">Movilidad Sostenible</option>
+              <option value="sanidad">Sanidad</option>
+              <option value="alumbrado">Alumbrado</option>
+              <option value="residuos-limpieza">
+                Residuos Urbanos y Limpieza
+              </option>
+              <option value="otros">Otros</option>
+            </select>
+          </div>
 
-          <Form.Item label="Hora del incidente">
-            <Input type="time" name="timeIncident" placeholder="Hora del incidente" required />
-          </Form.Item>
+          <div className="custom-input-file">
+            <input
+              className="input-file"
+              type="file"
+              name="imageIncident"
+              placeholder="image"
+            />
+            <CloudUploadOutlined /> Subir imágen
+          </div>
 
-          <Form.Item label="Categoría">
-            <Select name="category" placeholder="Seleccionar categoría" required>
-              <Select.Option value="jardineria">Jardinería</Select.Option>
-              <Select.Option value="movilidad-sostenible">Movilidad Sostible</Select.Option>
-<Select.Option value="sanidad">Sanidad</Select.Option>
-<Select.Option value="seguridad">Seguridad</Select.Option>
-<Select.Option value="otro">Otro</Select.Option>
-</Select>
-</Form.Item>
-<Form.Item label="Título">
-        <Input type="text" name="title" placeholder="Título del incidente" required />
-      </Form.Item>
+          <input type="text" name="locationIncident" placeholder="Ubicación" />
 
-      <Form.Item label="Descripción">
-        <Input.TextArea name="description" placeholder="Detalles del incidente" required />
-      </Form.Item>
+          <input type="text" name="title" placeholder="Título" />
 
-      <Form.Item label="Imagen del incidente">
-        <Input type="file" name="imagePost" placeholder="Imagen del incidente" accept="image/*" required />
-        <CloudUploadOutlined className="upload-icon" />
-      </Form.Item>
+          <textarea
+            name="description"
+            rows="20"
+            cols="26"
+            placeholder="Descripción del reporte"
+          />
 
-      <Form.Item label="Ubicación del incidente">
-        <Input type="text" name="locationIncident" placeholder="Ubicación del incidente" required />
-      </Form.Item>
-
-      <Button type="primary" htmlType="submit">
-        Enviar
-      </Button>
-    </form>
-  </div>
-</div>
-);
+          <Button
+            type="primary"
+            block
+            htmlType="submit"
+            className="addincident-form-button"
+          >
+            Publicar
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddIncident;
