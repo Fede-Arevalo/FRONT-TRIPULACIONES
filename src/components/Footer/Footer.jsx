10bfeb41b7889a951bce0adca0c3c3074ceaef9c
Fede-Arevalo <arevalo.feder@gmail.com>
2023-01-23 16:04:11 +0100

    footer con iconos iguales

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Footer.scss";
-import {
-  UserOutlined,
-  HomeOutlined,
-  CommentOutlined,
-  DeleteOutlined,
-} from "@ant-design/icons";
+import { UserOutlined, DeleteOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
+import Home from "../../assets/home.png";
+import Chat from "../../assets/Chat.png";
+import User from "../../assets/User.png";
 
 const Footer = () => {
   const { user } = useSelector((state) => state.auth);
@@ -16,11 +14,13 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      {location.pathname !== "/login" && location.pathname !== "/register" && location.pathname !== "/" ? (
+      {location.pathname !== "/login" &&
+      location.pathname !== "/register" &&
+      location.pathname !== "/" ? (
         <nav>
           <div className="inicio">
             <Link to="/wellcome">
-              <HomeOutlined />
+              <img src={Home} alt="Home" className="icon-home" />
             </Link>
             inicio
           </div>
@@ -29,13 +29,13 @@ const Footer = () => {
               <>
                 <div className="chat">
                   <Link to="/chatbot">
-                    <CommentOutlined />
+                    <img src={Chat} alt="Chat" className="icon-chat" />
                   </Link>
                   Chat
                 </div>
                 <div className="perfil">
                   <Link to="/profile">
-                    <UserOutlined />
+                    <img src={User} alt="User" className="icon-user" />
                   </Link>
                   Perfil
                 </div>
