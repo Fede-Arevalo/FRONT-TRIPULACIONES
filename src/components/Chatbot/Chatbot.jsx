601222c29a313be14b101ab192867333210094ff
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-25 03:40:57 +0100

    bugs fixeados

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -64,36 +64,3 @@ function Chatbot() {
 
 export default Chatbot;
 
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { askQuestion } from '../../features/chatbot/chatbotService';
-// import { useSelector } from 'react-redux'
-
-// function Chatbot() {
-//   const [question, setQuestion] = useState('');
-//   const response = useSelector(state => state.chatbot.response);
-//   const status = useSelector(state => state.chatbot.status);
-//   const error = useSelector(state => state.chatbot.error);
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     dispatch(askQuestion({ question }));
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         value={question}
-//         onChange={(e) => setQuestion(e.target.value)}
-//       />
-//       <button type="submit">Enviar</button>
-//       {status === 'loading' && <p>Loading...</p>}
-//       {status === 'succeeded' && <p>Respuesta: {response}</p>}
-//       {status === 'failed' && <p>Error: {error}</p>}
-//     </form>
-//   );
-// }
-
-// export default Chatbot;
