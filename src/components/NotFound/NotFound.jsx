8ae36403841fc497f26ec0e7d232e80dab1c651d
matias salvatore <[EMAIL]>
2023-01-25 20:09:03 +0100

    notfound

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import NF from "../../assets/images/NF404.png"
+import "./NotFound.scss"
+
+const NotFound = () => {
+  return (
+    <div>
+        <img src={NF} alt="NF" className="not-found-jpg"/>
+    </div>
+  )
+}
+
+export default NotFound
